4d368872c281d0027398649d73b45b3e96779051
AlexMitev21 <alex@takin.solutions>
2023-10-24 12:33:30 +0300

    first commit

--- website/pages.py
+++ website/pages.py
@@ -1,6 +1,8 @@
 '''
 Created on Mar. 11, 2021
-
+python -m flask --app ./website/main.py  run --host=0.0.0.0 --reload -p 8088
+* Running on http://127.0.0.1:8088
+* Running on http://192.168.100.6:8088
 @author: Pete Harris
 '''
 from flask import (
